aef1ef9f8076b0fe1eecedc2e6a8a247250b5061
Joan Renfroe <joanrenfroe@Joans-MacBook-Pro.local>
2022-04-02 21:06:17 -0700

    Greeting express application created.

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const PORT = 3000;
+
+
 
 //Routes
 
 app.get("/greeting/:name", (req, res) => {
-  res.send("Hello, stranger");
+    let greeting = (req.params.name);
+    console.log(req.params);
+    if (greeting) {
+        res.send("What's up, " + req.params.name + "!")
+    } else {
+        res.send("Hello, stranger") 
+    }
+ 
+});
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log("Express is listening on port " + PORT);
 });
